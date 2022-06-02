257a86820495cb6693c7f0f8cdbcdd54705d5d1c
Ilmira Khasanova <khasanovaII@comlogic.ru>
2022-06-02 16:29:02 +0700

    [TASK-34734]: Добавить CI/CD

---
+++ src/test/java/com/intabia/wikitabia/rest/DefualtTest.java
@@ -0,0 +1,13 @@
+package com.intabia.wikitabia.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DefualtTest {
+
+  @Test
+  void test() {
+    System.out.println("Тестируем...");
+  }
+}
